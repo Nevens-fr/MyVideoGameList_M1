8ffcb4ab1fa6ab452496065a333c51b82b803005
Nevens <aurelientudo@hotmail.fr>
2022-01-20 23:27:28 +0100

    Création des fichiers pour les différentes activités Ajout des maquettes

---
+++ MyVideoGameList/app/src/main/java/com/example/myvideogamelist/GamesListActivity.java
@@ -0,0 +1,14 @@
+package com.example.myvideogamelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games_list);
+    }
+}
---
+++ MyVideoGameList/app/src/main/java/com/example/myvideogamelist/UserGameRating.java
@@ -0,0 +1,14 @@
+package com.example.myvideogamelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserGameRating extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_game_rating);
+    }
+}
