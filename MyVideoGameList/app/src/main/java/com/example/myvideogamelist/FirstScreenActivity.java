45d3976413a0b526ff2a0696739a49c69142a24d
Jourry/Axel <Axel.jourry.etu@univ-lemans.fr>
2022-02-07 10:37:20 +0100

    Added home screen. Added Sign in screen. Added Log in screen.

---
+++ MyVideoGameList/app/src/main/java/com/example/myvideogamelist/FirstScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.myvideogamelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class firstScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen);
+    }
+}
